5b9a9d5aa467170ad434ee70d8f6e68bd5bf37d1
Roberto <cacaracteres86@gmail.com>
2022-11-29 12:50:00 +0100

    initial commit 2

--- src/db.connect.ts
+++ src/db.connect.ts
@@ -3,7 +3,7 @@ import { USER, PASSWORD } from './config.js';
 
 export async function dbConnect() {
     const DBName =
-        process.env.NODE_ENV !== 'test' ? 'Coders2022' : 'CodersTesting';
+        process.env.NODE_ENV !== 'test' ? 'Proyecto-Final' : 'testing';
     const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.yswmjv0.mongodb.net/?retryWrites=true&w=majority`;
     return mongoose.connect(uri);
 }
---
+++ src/dbConnect.test.ts
@@ -0,0 +1,27 @@
+import { dbConnect } from './db.connect';
+import mongoose from 'mongoose';
+
+const spiConnect = jest.spyOn(mongoose, 'connect');
+
+describe('Given "dbConnect"', () => {
+    describe('When the environment is "test"', () => {
+        test('Then it should connect with DB', async () => {
+            const result = await dbConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe('test');
+        });
+    });
+    describe('When the environment is not "test"', () => {
+        test('Then it should connect with DB', async () => {
+            process.env.NODE_ENV = 'development';
+            const result = await dbConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe('test');
+        });
+    });
+    afterEach(() => {
+        mongoose.disconnect();
+    });
+});
